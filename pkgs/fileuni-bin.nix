d9b29221ea3fc2c13d2048f8d704c75133b79b96
FileUni <git@fileuni.com>
2026-03-16 10:06:10 +0000

    chore(release): update package for 0.0.4-alpha.4.20260316172913

--- pkgs/fileuni-bin.nix
+++ pkgs/fileuni-bin.nix
@@ -2,12 +2,12 @@
 
 let
   pname = "fileuni";
-  version = "0.0.4-alpha.3.20260316152346";
+  version = "0.0.4-alpha.4.20260316172913";
   sources = {
-    "x86_64-linux" = { url = "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.3_20260316152346/FileUni-cli-x86_64-linux-gnu.zip"; hash = "sha256-EBKZlUt1uX74vOuPTj2rOVVDC19mHT373Fs87IkCLwo="; };
-    "aarch64-linux" = { url = "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.3_20260316152346/FileUni-cli-aarch64-linux-gnu.zip"; hash = "sha256-Q1XFtVDEPDNkduFIzAAS0iVmhCWWDGjZy44Z+pvfYOc="; };
-    "x86_64-darwin" = { url = "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.3_20260316152346/FileUni-cli-x86_64-macos-darwin.zip"; hash = "sha256-JNRl2P6UYfdcusBx2dov/uENEKcXKNs3trP6Ywzs8cs="; };
-    "aarch64-darwin" = { url = "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.3_20260316152346/FileUni-cli-aarch64-macos-darwin.zip"; hash = "sha256-Dcrlk8Nk8zJCleV/ryZ+0T9i0tUhad2uIol+eYd1Fu8="; };
+    "x86_64-linux" = { url = "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.4_20260316172913/FileUni-cli-x86_64-linux-gnu.zip"; hash = "sha256-wFQv0InUqRPlaBsanNlbrf/HLCFk4aVVKBfbSqU39XI="; };
+    "aarch64-linux" = { url = "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.4_20260316172913/FileUni-cli-aarch64-linux-gnu.zip"; hash = "sha256-ZRED2f2thd3eVVrjTGVuJtXsnIdbxAG8P0VGJzdOsHw="; };
+    "x86_64-darwin" = { url = "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.4_20260316172913/FileUni-cli-x86_64-macos-darwin.zip"; hash = "sha256-Bi+wnhjzVT39QLUaS6vcx1Liiy9P39CM43r+7vPRVZA="; };
+    "aarch64-darwin" = { url = "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.4_20260316172913/FileUni-cli-aarch64-macos-darwin.zip"; hash = "sha256-IB+OmBR+tqPUfXrJg0rxc//a9kya9mego/+5168tasM="; };
   };
   source = sources.${stdenvNoCC.hostPlatform.system}
     or (throw "Unsupported system for FileUni: ${stdenvNoCC.hostPlatform.system}");
